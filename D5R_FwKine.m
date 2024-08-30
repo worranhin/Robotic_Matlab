0a5be2eb9a2d0d9f68d553cbe458bc8353f01148
worranhin <worranhin@foxmail.com>
2024-08-30 20:21:43 +0800

    Complete: Jacobi analys; Ongoing: Trajectory planning.

--- D5R_FwKine.m
+++ D5R_FwKine.m
@@ -3,9 +3,15 @@ function [px, py, pz, ry, rz] = D5R_FwKine(r1, p2, p3, p4, r5)
 %   r1, r5 单位为 °
 %   p2, p3, p4, p5 单位为 mm
 
-px = 35.35 * sind(r1) + p3 * cosd(r1) + p2 * sind(r1);
-py = -35.35 * cosd(r1) + p3 * sind(r1) - p2 * cosd(r1);
-pz = -p4 - 77.5;
+l1 = 38;
+l2 = 11.5;
+l3 = 17.25;
+l4 = 28;
+l5 = 18.1;
+
+px = (l3+l5) * sind(r1) + p3 * cosd(r1) + p2 * sind(r1);
+py = -(l3+l5) * cosd(r1) + p3 * sind(r1) - p2 * cosd(r1);
+pz = -p4 - (l1+l2+l4);
 ry = r1;
 rz = r5;
 end
